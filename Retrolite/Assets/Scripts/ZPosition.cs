5b7fb64999d7e32bea0a3a64aa87face7d7b38a8
AnDenL <andenl145@gmail.com>
2024-05-29 10:43:32 +0300

    Start

--- Retrolite/Assets/Scripts/ZPosition.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class ZPosition : MonoBehaviour
-{
-    void Start()
-    {
-        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.y);
-    }
-}
