cdc815d9d9e9accd40dfbf9dd8e665e22332fa0c
AnDenL <andenl145@gmail.com>
2026-01-27 13:50:01 +0200

    Fix

--- Retrolite/Assets/Scripts/Generation/MaterialSetSeed.cs
+++ Retrolite/Assets/Scripts/Generation/MaterialSetSeed.cs
@@ -5,5 +5,5 @@ public class MaterialSetSeed : MonoBehaviour
     [SerializeField] private LevelGenerationBase generator;
     [SerializeField] private Material material;
 
-    private void Start() => material.SetFloat("_Seed", generator.seed);
+    private void Start() => material.SetFloat("_Seed", (float)generator.seed / uint.MaxValue);
 }
